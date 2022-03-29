a10bf45548b712220429671fddeeab2072233bc4
TK-Rev <100632145+TK-Rev@users.noreply.github.com>
2022-03-29 11:59:48 -0400

    Standings Refresher Complete
    All refreshers are good to go, now to just get the queries running. Started working on users system.

--- src/main/scala/Sparky.scala
+++ src/main/scala/Sparky.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.sql.SparkSession
 import scala.io.Source
 import java.io._
+import scala.io.StdIn.readLine
 
 object Sparky {
 
@@ -290,6 +291,102 @@ object Sparky {
     util.sql("CREATE TABLE teams(ID Int,Team String,ABB String,Location String,Name String) row format delimited fields terminated by ','")
     util.sql("LOAD DATA LOCAL INPATH 'teamsFile.csv' INTO TABLE teams")
   }
+  def RefStandings(util:SparkSession): Unit ={
+    val html = Source.fromURL("https://statsapi.web.nhl.com/api/v1/teams?expand=team.stats")
+    val s = html.mkString
+    val resu = s.split("\\\n")
+
+    var cur = ""
+    var tote = ""
+
+    var idtemp = ""
+    var id = 0
+    var conf = ""
+    var div = ""
+    var gp = 0
+    var w = 0
+    var l = 0
+    var otl = 0
+    var pts = 0
+    var gpg = 0.0
+    var gapg = 0.0
+
+    resu.foreach(e=>{
+      val f = e.trim
+      if(f.contains("\"team\"")&&cur=="stat"){
+        cur = "team"
+      }else if(f.contains("\"division\"")){
+        cur = "division"
+      }else if(f.contains("\"conference\"")){
+        cur = "conference"
+      }else if(f.contains("\"splits\"")){
+        cur = "stat"
+      }else if(f.contains("}, {")){
+        cur = ""
+      }
+
+      cur match {
+        case "division" => {
+          if(f.contains("\"name\"")){
+            div = f.split(":")(1).trim
+            div = div.substring(1,div.length-2)
+          }
+        }
+        case "conference" => {
+          if(f.contains("\"name\"")){
+            conf = f.split(":")(1).trim
+            conf = conf.substring(1,conf.length-2)
+          }
+        }
+        case "stat" => {
+          if(f.contains("\"gamesPlayed\"")){
+            idtemp = f.split(":")(1).trim
+            gp = idtemp.substring(0,idtemp.length-1).toInt
+          }else if(f.contains("\"wins\"")){
+            idtemp = f.split(":")(1).trim
+            w = idtemp.substring(0,idtemp.length-1).toInt
+          }else if(f.contains("\"losses\"")){
+            idtemp = f.split(":")(1).trim
+            l = idtemp.substring(0,idtemp.length-1).toInt
+          }else if(f.contains("\"ot\"")){
+            idtemp = f.split(":")(1).trim
+            otl = idtemp.substring(0,idtemp.length-1).toInt
+          }else if(f.contains("\"pts\"")){
+            idtemp = f.split(":")(1).trim
+            pts = idtemp.substring(0,idtemp.length-1).toInt
+          }else if(f.contains("\"goalsPerGame\"")){
+            idtemp = f.split(":")(1).trim
+            gpg = idtemp.substring(0,idtemp.length-1).toDouble
+          }else if(f.contains("\"goalsAgainstPerGame\"")){
+            idtemp = f.split(":")(1).trim
+            gapg = idtemp.substring(0,idtemp.length-1).toDouble
+          }
+        }
+        case "team" => {
+          if(f.contains("\"id\"")){
+            idtemp = f.split(":")(1).trim
+            id = idtemp.substring(0,idtemp.length-1).toInt
+
+            if(tote!=""){
+              tote+=s"\n$id,$conf,$div,$gp,$w,$l,$otl,$pts,$gpg,$gapg"
+            }else{
+              tote+=s"$id,$conf,$div,$gp,$w,$l,$otl,$pts,$gpg,$gapg"
+            }
+          }
+        }
+        case _ => // do nothing
+      }
+    })
+
+    val file = new File("standingsFile.csv")
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(tote)
+    bw.close()
+
+    util.sql("DROP TABLE IF EXISTS standings")
+    util.sql("CREATE TABLE standings(TeamID int,Conference String,Division String,GP int,W int,L int,OTL int,PTS int,GPG double,GAPG double) row format delimited fields terminated by ','")
+    util.sql("LOAD DATA LOCAL INPATH 'standingsFile.csv' INTO TABLE standings")
+  }
 
   def Refresher(util:SparkSession,cate:String): Unit ={
     cate match {
@@ -297,12 +394,15 @@ object Sparky {
       case "next game" => RefNextGames(util)
       case "last game" => RefPastGames(util)
       case "roster" => RefRoster(util)
-      case "standings" => //
+      case "standings" => RefStandings(util)
       case "all" => {
         RefTeams(util)
         RefRoster(util)
         //RefNextGames(util)
+        // This is commented out because I don't want to refresh this until presentation day.
+        // To demonstrate that the program auto-updates information when requested to.
         RefPastGames(util)
+        RefStandings(util)
       }
     }
   }
@@ -321,6 +421,46 @@ object Sparky {
     Standings (TeamID,Wins,Losses,OTL,Points)
    */
 
+  def Login(util:SparkSession): Unit ={
+
+  }
+  def NewUser(util:SparkSession): Unit ={
+    var user = ""
+    var pass = ""
+    var topy = ""
+    var tz = ""
+    // Username, Password, Type, Timezone
+    println("Create Username;")
+    user = readLine
+    println("Create password;")
+    pass = readLine
+
+    do {
+      println("Basic or Admin User?")
+      readLine.toLowerCase match {
+        case "basic" || "basic user" || "bu" => topy = "basic"
+        case "admin" || "admin user" || "au" => topy = "admin"
+        case _ => println("Unclear. Try again.")
+      }
+    } while(topy=="")
+
+    do {
+      println("Your timezone?")
+      println("[EST | CST | MST | PST]")
+      readLine.toLowerCase match {
+        case "est" => tz = "est"
+        case "cst" => tz = "cst"
+        case "mst" => tz = "mst"
+        case "pst" => tz = "pst"
+        case _ => println("Unclear. Try again.")
+      }
+    } while(tz=="")
+
+    println("Verifying...")
+    
+
+  }
+
   def main(args: Array[String]): Unit = {
     System.setProperty("hadoop.home.dir","C:\\hadoop")
     val spark = SparkSession
@@ -334,7 +474,7 @@ object Sparky {
     spark.sparkContext.setLogLevel("ERROR")
 
     Refresher(spark,"all")
-    spark.sql("SELECT teams.Team, teams.ABB, teams.Location, teams.Name FROM teams").show(32,false)
+  /*  spark.sql("SELECT teams.Team, teams.ABB, teams.Location, teams.Name FROM teams").show(32,false)
     // spark.sql("SELECT * FROM players").show(900, false)
     spark.sql("SELECT players.FullName, players.Position, players.JerseyNumber, teams.Team FROM players INNER JOIN teams ON players.TeamID = teams.ID").show(false)
     spark.sql("SELECT DISTINCT nextGames.Date, teams.Team as Away, teams2.Team as Home, nextGames.est as Time FROM nextGames " +
@@ -347,5 +487,12 @@ object Sparky {
       "INNER JOIN teams ON pastGames.AwayID == teams.ID " +
       "INNER JOIN teams as teams2 ON pastGames.HomeID == teams2.ID " +
       "ORDER BY pastGames.Date").show(false)
+    spark.sql("SELECT teams.Team, standings.Conference, standings.Division, standings.GP, standings.W, standings.L, standings.OTL, standings.PTS, " +
+      "standings.GPG, standings.GAPG FROM standings " +
+      "INNER JOIN teams ON standings.TeamID == teams.ID ORDER BY standings.PTS DESC").show(32,false) */
+
+    // THE ABOVE IS LIKE THIS BECAUSE I AM NOW WORKING ON IMPLEMENTING THE INTERACTION SYSTEM
+
+
   }
 }
